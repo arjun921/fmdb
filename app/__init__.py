b4b20f7352bcbcb33f817411e7c2675f45c948af
Arjun S <arjun921@gmail.com>
2019-04-12 07:03:51 +0530

    Updated config for prod and dev environments

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import os
 
 app = Flask(__name__, static_url_path='/static', instance_relative_config=True)
 
 from app import views
 
-app.config.from_object('config')
+app.config.from_object(os.environ.get('APP_SETTINGS','config.DevelopmentConfig'))
--- config.py
+++ config.py
@@ -1,5 +1,27 @@
-# config.py
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable Flask's debugging features. Should be False in production
-DEBUG = True
-HOST='0.0.0.0'
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.get('SECRET_KEY')
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
