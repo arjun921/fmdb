a3536d0691be6e8199bd284a3ce62655223f45e8
Arjun S <arjun921@gmail.com>
2019-04-15 00:02:44 +0530

    Basic manage movies APIs added

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask_user import (UserManager, UserMixin, current_user, login_required,
 from app import app
 from utils.database import (create_connection, select_all,
                             select_all_where_value_matches,
-                            select_all_with_similar_value)
+                            select_all_with_similar_value, create_by_id)
 from utils.decorators import timeit
 from utils.helpers import log_line
 
@@ -32,43 +32,46 @@ def member_page():
     DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
     DB_URI = DB_URI.split('///')[1]
     db_conn = create_connection(DB_URI)
-    
+
     # select all for search autocomplete.
-    movie_list = select_all(db_conn,'movie_data')
+    movie_list = select_all(db_conn, 'movie_data')
     searched = False
 
     # search
     if 'name' in request_query:
-        search_results = select_all_with_similar_value(db_conn,db='movie_data',field='name',matches=request_query['name'])
+        search_results = select_all_with_similar_value(
+            db_conn, db='movie_data', field='name', matches=request_query['name'])
         searched = True
     elif 'director' in request_query:
-        search_results = select_all_with_similar_value(db_conn,db='movie_data',field='director',matches=request_query['director'])
+        search_results = select_all_with_similar_value(
+            db_conn, db='movie_data', field='director', matches=request_query['director'])
         searched = True
     elif 'genre' in request_query:
-        search_results = select_all_with_similar_value(db_conn,db='movie_data',field='genre',matches=request_query['genre'])
+        search_results = select_all_with_similar_value(
+            db_conn, db='movie_data', field='genre', matches=request_query['genre'])
         searched = True
-    
+
     # pagination
-    page = int(request_query.get('p',"0"))
-    num_items = int(request_query.get('items',app.config.get('MAX_LISTING_ITEMS','50')))
+    page = int(request_query.get('p', "0"))
+    num_items = int(request_query.get(
+        'items', app.config.get('MAX_LISTING_ITEMS', '50')))
     start = page*num_items
-    
-    if start==0:
+
+    if start == 0:
         # if start zero, set slice end with number of items per page
         end = num_items
     else:
         # add number of items to start position
         end = start+(num_items)
-    
-    #assign to common variable for reducing duplicate code
+
+    # assign to common variable for reducing duplicate code
     if searched:
         render_movies = search_results
     else:
         render_movies = movie_list
 
     max_pages = floor(len(render_movies)/num_items)
-    return render_template("listing.html",movie_list=render_movies[start:end],current_page=page,max_pages=max_pages,full_movie_list=movie_list)
-
+    return render_template("listing.html", movie_list=render_movies[start:end], current_page=page, max_pages=max_pages, full_movie_list=movie_list)
 
 
 @app.route('/admin')
@@ -78,14 +81,32 @@ def admin_page():
     DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
     DB_URI = DB_URI.split('///')[1]
     db_conn = create_connection(DB_URI)
-    
+
     # select all for search autocomplete.
-    full_movie_list = select_all(db_conn,'movie_data')
-    return render_template('admin.html',full_movie_list=full_movie_list)
+    full_movie_list = select_all(db_conn, 'movie_data')
+    return render_template('admin.html', full_movie_list=full_movie_list)
 
 
-# @app.route('/delete')
-# @roles_required('Admin')
-# def delete_entry():
-#     """DESTRUCTIVE FUNCTION AHEAD. Function in app.views"""
-    
+@app.route('/manage/delete/<int:id>', methods=['DELETE'])
+@roles_required('Admin')
+def delete_entry(id):
+    """DESTRUCTIVE FUNCTION AHEAD. Function in app.views"""
+    DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
+    DB_URI = DB_URI.split('///')[1]
+    db_conn = create_connection(DB_URI)
+    returned = delete_by_id(db_conn, 'movie_data', id)
+    if returned == 'Success':
+        return 'Success'
+
+
+@app.route('/manage/add', methods=['POST'])
+@roles_required('Admin')
+def add_movie_entry():
+    """DESTRUCTIVE FUNCTION AHEAD. Function in app.views"""
+    req_body = request.get_json(silent=True, force=True)
+    DB_URI = app.config['SQLALCHEMY_DATABASE_URI']
+    DB_URI = DB_URI.split('///')[1]
+    db_conn = create_connection(DB_URI)
+    returned = create_by_id(db_conn, 'movie_data', req_body)
+    if returned == 'Success':
+        return 'Success'
--- utils/database.py
+++ utils/database.py
@@ -6,41 +6,65 @@ from utils.decorators import timeit
 
 logger = logging.getLogger(__name__)
 
+
 @timeit
 def create_connection(db_uri):
     """Creating db connection in utils.database"""
-    log_line(logger,msg=db_uri)
+    log_line(logger, msg=db_uri)
     try:
         conn = sqlite3.connect(db_uri)
         return conn
     except Error as e:
-        log_line(logger,level='critical',msg=str(e),trace=False,exit=True)
+        log_line(logger, level='critical', msg=str(e), trace=False, exit=True)
+
 
 @timeit
-def select_all(conn,db):
+def select_all(conn, db):
     """Creating db connection in utils.database"""
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM {db}")
     rows = cur.fetchall()
+    conn.close()
     return rows
 
 
 @timeit
-def select_all_where_value_matches(conn,db,field,match_string):
+def select_all_where_value_matches(conn, db, field, match_string):
     """Creating db connection in utils.database"""
     cur = conn.cursor()
     cur.execute(f'SELECT * FROM {db} WHERE {field}="{match_string}"')
     rows = cur.fetchall()
+    conn.close()
     return rows
 
+
 @timeit
-def select_all_with_similar_value(conn,db,field,matches):
+def select_all_with_similar_value(conn, db, field, matches):
     """Creating db connection in utils.database"""
     cur = conn.cursor()
     matches = matches.lower()
     matches = '%'+'%'.join(matches.split(' '))+'%'
-    query=f'SELECT * FROM {db} WHERE {field} like "{matches}"'
-    print(query)
+    query = f'SELECT * FROM {db} WHERE {field} like "{matches}"'
     cur.execute(query)
     rows = cur.fetchall()
+    conn.close()
     return rows
+
+
+def delete_by_id(conn, db, id):
+    cur = conn.cursor()
+    query = f'DELETE FROM {db} WHERE id=?'
+    cur.execute(query, (id,))
+    conn.commit()
+    conn.close()
+    return 'Success'
+
+
+def create_by_id(conn, db, params):
+    cur = conn.cursor()
+    query = f"INSERT INTO movie_data(popularity,director,genre,imdb_score,name) VALUES ({float(params['popularity'])}, '{params['director']}','{params['chip_genres']}',{float(params['imdb_score'])},'{params['movie_name']}')"
+    print(query)
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+    return 'Success'
